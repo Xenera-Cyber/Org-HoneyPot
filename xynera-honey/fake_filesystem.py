dcc107f3005a051a7a30e76e203b90e128f8468c
deshwalaayush05-pixel <deshwalaayush05@gmail.com>
2026-06-26 14:43:50 +0530

    make linux server more realstic

--- xynera-honey/fake_filesystem.py
+++ xynera-honey/fake_filesystem.py
@@ -95,11 +95,43 @@ mysql -u root
 
     "/etc/passwd": """
 root:x:0:0:root:/root:/bin/bash
-ubuntu:x:1000:1000::/home/ubuntu:/bin/bash
-dev:x:1001:1001::/home/dev:/bin/bash
-backup:x:1002:1002::/home/backup:/bin/bash
+daemon:x:1:1:daemon:/usr/sbin:/usr/sbin/nologin
+bin:x:2:2:bin:/bin:/usr/sbin/nologin
+sys:x:3:3:sys:/dev:/usr/sbin/nologin
+sync:x:4:65534:sync:/bin:/bin/sync
+games:x:5:60:games:/usr/games:/usr/sbin/nologin
+man:x:6:12:man:/var/cache/man:/usr/sbin/nologin
+lp:x:7:7:lp:/var/spool/lpd:/usr/sbin/nologin
+mail:x:8:8:mail:/var/mail:/usr/sbin/nologin
+news:x:9:9:news:/var/spool/news:/usr/sbin/nologin
+uucp:x:10:10:uucp:/var/spool/uucp:/usr/sbin/nologin
+proxy:x:13:13:proxy:/bin:/usr/sbin/nologin
 www-data:x:33:33:www-data:/var/www:/usr/sbin/nologin
-mysql:x:111:115:MySQL Server:/nonexistent:/bin/false
+backup:x:34:34:backup:/var/backups:/usr/sbin/nologin
+list:x:38:38:Mailing List Manager:/var/list:/usr/sbin/nologin
+irc:x:39:39:ircd:/run/ircd:/usr/sbin/nologin
+gnats:x:41:41:Gnats Bug Reporting:/var/lib/gnats:/usr/sbin/nologin
+nobody:x:65534:65534:nobody:/nonexistent:/usr/sbin/nologin
+systemd-network:x:100:102:Systemd Network Management:/:/usr/sbin/nologin
+systemd-timesync:x:101:103:Systemd Time Synchronization:/:/usr/sbin/nologin
+messagebus:x:102:106:D-Bus Message Bus:/nonexistent:/usr/sbin/nologin
+syslog:x:103:110:Syslog:/home/syslog:/usr/sbin/nologin
+_apt:x:104:65534::/nonexistent:/usr/sbin/nologin
+uuidd:x:105:111:UUID Daemon:/run/uuidd:/usr/sbin/nologin
+tcpdump:x:106:112::/nonexistent:/usr/sbin/nologin
+tss:x:107:113:TPM Software Stack:/var/lib/tpm:/bin/false
+landscape:x:108:114:Landscape Client:/var/lib/landscape:/usr/sbin/nologin
+pollinate:x:109:1:Pollinate:/var/cache/pollinate:/bin/false
+mysql:x:110:115:MySQL Server:/nonexistent:/bin/false
+postgres:x:111:116:PostgreSQL Administrator:/var/lib/postgresql:/bin/bash
+redis:x:112:117:Redis Server:/var/lib/redis:/usr/sbin/nologin
+docker:x:113:118:Docker Engine:/var/lib/docker:/usr/sbin/nologin
+nginx:x:114:119:Nginx Service:/var/cache/nginx:/usr/sbin/nologin
+ubuntu:x:1000:1000:Ubuntu User:/home/ubuntu:/bin/bash
+dev:x:1001:1001:Developer:/home/dev:/bin/bash
+admin:x:1002:1002:System Administrator:/home/admin:/bin/bash
+support:x:1003:1003:Support Engineer:/home/support:/bin/bash
+backupuser:x:1004:1004:Backup Operator:/home/backupuser:/bin/bash
 """,
 
     "/etc/hosts": """
